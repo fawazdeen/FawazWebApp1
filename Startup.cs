9ddbb4fa6f411101d3da2cdfa6f5fc111e839cbb
fawazdeen <fawazdeen@gmail.com>
2016-11-10 14:01:39 +1100

    Initial Projectr

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FawazWebApp1.Startup))]
+namespace FawazWebApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
